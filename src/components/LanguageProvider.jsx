f8760f790a98fefe1d8725eb114cfeb6ced38566
Patricia E. Powers <patricia.e.powers@gmail.com>
2026-02-06 22:27:42 -0500

    Created LanguageProvider.jsx

---
+++ src/components/LanguageProvider.jsx
@@ -0,0 +1,15 @@
+const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState("en");
+
+  const toggleLanguage = () => {
+    setLanguage((prev) => (prev === "en" ? "fr" : "en"));
+  };
+
+  const t = translations[language];
+
+  return (
+    <LanguageContext.Provider value={{ language, toggleLanguage, t }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
