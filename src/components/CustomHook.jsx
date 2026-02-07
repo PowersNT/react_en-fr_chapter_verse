2282d0c323975595c0ef483d98979c1350410871
Patricia E. Powers <patricia.e.powers@gmail.com>
2026-02-06 22:23:20 -0500

    Created CustomHook.jsx

---
+++ src/components/CustomHook.jsx
@@ -0,0 +1,7 @@
+const useLanguage = () => {
+  const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error("useLanguage must be used within LanguageProvider");
+  }
+  return context;
+};
